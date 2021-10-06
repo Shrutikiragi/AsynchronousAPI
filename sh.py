d5ae181498c005367d60afe44ad9a9964c5fefb9
shrutikiragi <shrutikiragi93@gmail.com>
2021-10-06 15:24:57 +0530

    text file

--- sh.py
+++ sh.py
@@ -1,16 +1,29 @@
-from flask import Flask, url_for
+from multiprocessing.pool import ThreadPool
+
+
+from flask import Flask, request
+from multiprocessing.pool import ThreadPool
+
 app = Flask(__name__)
-@app.route('/')
-def index(): pass
 
-@app.route('/login')
-def login(): pass
+@app.route('/', methods=['GET'])
+def home():
+    return 'Home page running'
 
-@app.route('/user/<username>')
-def profile(username): pass
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
-with app.test_request_context():
-  print(url_for('index'))
-  print(url_for('login'))
-  print(url_for('login', next='/'))
-  print (url_for('profile', username='John Doe'))
+
+@app.route('/api/tasks')
+def tasks(appService=None):
+    pool = ThreadPool(processes=1)
+    async_result = pool.apply_async(appService.get_tasks)
+    return async_result.get()
+
+@app.route('/api/task', methods=['POST'])
+def create_task(appService=None):
+    request_data = request.get_json()
+    task = request_data['task']
+    pool = ThreadPool(processes=1)
+    async_result = pool.apply_async(appService.create_task, (task,))
+    return async_result.get()
